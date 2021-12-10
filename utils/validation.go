2b4c3a35aaa198d621cd1d7be66e94032b0ffa13
mor <xmor61@users.sourceforge.net>
2021-12-09 22:40:04 -0800

    fix debug msgs

--- utils/validation.go
+++ utils/validation.go
@@ -1,14 +1,14 @@
 package utils
 
 import (
-        "fmt"
+        "log"
 	"regexp"
 	"strings"
 )
 
 func IsOnion(identifier string) bool {
 	// TODO: At some point we will want to support i2p
-	fmt.Printf("parse .onion string: %s", identifier)
+	log.Printf("parse .onion string: %s", identifier)
 	if len(identifier) >= 22 && strings.HasSuffix(identifier, ".onion") {
 		matched, _ := regexp.MatchString(`(^|\.)[a-z2-7]{56}\.onion$`, identifier)
 		return matched
